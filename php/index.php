59ee62e2192130fe40657917797b27495b7be0c4
malcolmmyles <malcolmmyles@live.com>
2016-05-25 18:07:42 -0700

    5-25 evening commit

---
+++ php/index.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<form action="index.php" method="POST">
+		Name:<br><input type="text" name="contact name"><br><br>
+		Email address:<br><input type="text" name ="malcolmmyles@live.com"><br><br>
+		Message:<br>
+    <textarea name ="contact_text" rows="6" cols="30"></textarea></br></br>
+    <input type ="submit" value="Send">
+</form>
